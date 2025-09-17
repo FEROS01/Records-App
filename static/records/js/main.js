d2584deb1345d9fcf4d7f73363c23c89ffc5964d
Ope <opeoluwaferanmi89@gmail.com>
2025-09-17 22:09:33 +0100

    Hamburger design

--- static/records/js/main.js
+++ static/records/js/main.js
@@ -1,1 +1,11 @@
-console.log('')
+let overlayDiv = document.querySelector(".overlay_box")
+
+const toggleOverlay = () => {
+    overlayDiv.classList.toggle("active");
+}
+
+let hamburger = document.querySelector(".icon.hamburger")
+let cancel = document.querySelector(".overlay_box .cancel")
+
+hamburger.addEventListener('click', toggleOverlay)
+cancel.addEventListener('click', toggleOverlay)
